cfabe90dc1e8aaf1bfa4b2f73e2390f8562c6cf5
gyk <1843773386@qq.com>
2021-03-22 09:00:45 +0800

    mybatis初步工作

--- src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -1,13 +1,27 @@
 package org.csu.mypetstore;
 
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.service.CatelogService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@MapperScan("org.csu.mypetstore.persistence")
 class MypetstoreApplicationTests {
+
+    @Autowired
+    CatelogService catelogService;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        Category c = catelogService.getCategory("birds");
+        System.out.println(c.getDescription());
     }
 
 }
